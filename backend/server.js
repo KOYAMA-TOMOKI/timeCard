30fe983201b0da5d6d2dee167698a7e5ca7d5361
koKOYAMA-TOMOKI <maizuki58@gmail.com>
2025-03-09 01:59:08 +0900

    途中経過

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,37 @@
-//Node.jsにExpressを読み込み
-//CORSを使う
+//Node.jsにExpressを読み込み（サーバー作成）
+//CORSを使う（ミドルウェアの読み込み）
 const express = require('express');
 const cors = require('cors');
-//Expressアプリの作成
+
+//Expressサーバーの作成
 const app = express();
 
 //フロントエンドからの通信
-app.use(cors()); //cors有効化
+//corsの設定(環境ごとに変わるようにする)
+app.use(cors({
+    origin: function (origin, callback){ //orginにフロントURLが入る
+        if(!origin){
+            callback(null, true); //許可
+        } else {
+            callback(null, origin); //許可なしならエラー
+        }
+    },
+    methods: ['GET','POST'], //GETとPOSTのみ許可
+    credentials: true
+})); //cors有効化
+
 app.use(express.json()); //JSONリクエストを有効
 
-//APIエンドポイント
-app.get('/',(req,res) => {
-    res.send('サーバが正常に動作している');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
-//aaaaaa
+//APIエンドポイント
+/*app.get('/',(req,res) => {
+    res.send('サーバが正常に動作している');
+});
+
+*/
