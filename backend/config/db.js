26555add391dcae6dbaefc804ebabcd59b59e27a
koKOYAMA-TOMOKI <maizuki58@gmail.com>
2025-03-11 05:04:17 +0900

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,20 @@ const { Pool } = require('pg');
 //.envファイルを読み込む
 require('dotenv').config();
 
-const Pool = new Pool({
+const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.PASSWORD,
-    port: process.env.DB_PORT || 5432, //最初は5432
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT || 5432, // 最初は5432
 });
+
+pool.connect();
+pool.query("SELECT NOW()", (err, res) => {
+    pool.log(err, res);
+    client.end();
+  });
+
+
 
 module.exports = pool;
