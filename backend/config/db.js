ce0af72952fcbda3e1cff6e7b87a21c7d4ab040a
koKOYAMA-TOMOKI <maizuki58@gmail.com>
2025-03-17 00:26:08 +0900

    改変

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 //データベースについて
-const { Pool } = require('pg');
+const { Pool } = require('pg'); //接続プール作成のクラス
 //.envファイルを読み込む
-require('dotenv').config();
+require('dotenv').config(); //.envファイルを読み込んで環境変数を設定
 
-const pool = new Pool({
+const pool = new Pool({ // インスタンス（実体）
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -11,12 +11,14 @@ const pool = new Pool({
     port: process.env.DB_PORT || 5432, // 最初は5432
 });
 
-pool.connect();
+//テスト
 pool.query("SELECT NOW()", (err, res) => {
-    pool.log(err, res);
-    client.end();
+    if(err){
+        console.error('Error:',err); //エラーが発生した場合
+    }else{
+        console.log('Current time fromDB:', res.rows[0].now); //エラーが無ければ
+    }    
+    pool.end();
   });
 
-
-
-module.exports = pool;
+module.exports = pool; //エクスポート
