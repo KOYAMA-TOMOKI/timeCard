0179773fcaa1d8221ea1b004182349b144d8313a
koKOYAMA-TOMOKI <maizuki58@gmail.com>
2025-03-22 21:23:03 +0900

    DB開発用・実装用区別

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,8 +12,23 @@ const pool = new Pool({ // インスタンス（実体）
     port: process.env.DB_PORT || 5432, // 最初は5432
 });
 
+//テスト・実行別ける
+async function testConnection() {
+    try{
+        const res = await pool.query("SELECT NOW()");
+        console.log("DB接続成功:",res.rows[0].now); 
+    } catch (err) {
+        console.error("DB接続失敗:",err);
+    }
+}
+
+// NODE_ENV が production（本番）でないときだけ実行
+if (process.env.NODE_ENV !== 'production') {
+    testConnection(); // ← 本番では実行されない
+}
+
 //テストの実行
-pool.query("SELECT NOW()", (err, res) => {
+pool.query("SELECT NOW()", (err, res) => { //現在時刻を取るだけのクエリ（問い合わせ）
     if(err){
         console.error('Error:',err); //エラーが発生した場合
     }else{
@@ -22,4 +37,4 @@ pool.query("SELECT NOW()", (err, res) => {
     pool.end(); //プールを閉じる
   });
 
-module.exports = pool; //エクスポート（他のファイルでも使えるようにする）
+module.exports = pool; //エクスポート（他のファイルでも使えるようにする）　
